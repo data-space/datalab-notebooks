350f002370ed8b6b00073854ffca3293fe16d58a
David Oury <doury@bentley.edu>
2019-01-16 15:43:32 -0500

    first push

---
+++ Python/1. Essentials/0.0 Contents.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+# MAGIC %md # Contents - Python Essentials
+
+# COMMAND ----------
+
+# MAGIC %md List of notebooks in this folder with links:
+# MAGIC - `1.0 Basics` [Databricks](https://bentley.cloud.databricks.com/#notebook/90445)
+# MAGIC - `2.1 Lists` [Databricks](https://bentley.cloud.databricks.com/#notebook/90524)
+# MAGIC - `2.2 Tuples` [Databricks](https://bentley.cloud.databricks.com/#notebook/90494)
+# MAGIC - `2.3 Dictionaries` [Databricks](https://bentley.cloud.databricks.com/#notebook/90420)
+# MAGIC - `2.4 JSON` [Databricks](https://bentley.cloud.databricks.com/#notebook/858024)
+# MAGIC - `3.1 Comprehensions` [Databricks](https://bentley.cloud.databricks.com/#notebook/90595)
+# MAGIC - `3.2 Map, Filter, Reduce` [Databricks](https://bentley.cloud.databricks.com/#notebook/90384)
+# MAGIC - `3.3 Functions` [Databricks](https://bentley.cloud.databricks.com/#notebook/90341)
+# MAGIC - `4.1 Objects` [Databricks](https://bentley.cloud.databricks.com/#notebook/90404)
+# MAGIC - `4.2 Classes` [Databricks](https://bentley.cloud.databricks.com/#notebook/191802)
+# MAGIC - `6.1 Packages` [Databricks](https://bentley.cloud.databricks.com/#notebook/90573)
+# MAGIC - `7.1 Pipelines` [Databricks](https://bentley.cloud.databricks.com/#notebook/409455)
+# MAGIC - `7.2 Transformers` [Databricks](https://bentley.cloud.databricks.com/#notebook/430288)
+# MAGIC - `8.0 Exceptions` [Databricks](https://bentley.cloud.databricks.com/#notebook/90515)
+# MAGIC 
+# MAGIC Note: these "Databricks" links are for internal Bentley users.
